ebf373ebfe15db5a20cae01a87de4e52ab954755
Aleksandr Khodos <60875080+khodozzz@users.noreply.github.com>
2021-10-21 10:50:40 +0300

    test

--- launch.sh
+++ launch.sh
@@ -6,11 +6,11 @@ set -e
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
 
-roslaunch circle_drive indefinite_navigation.launch
+#roslaunch circle_drive circle_drive.launch
 
 #roslaunch duckietown_demos lane_following.launch
 
-#roslaunch --wait circle_drive indefinite_navigation2.launch &
-#sleep 5
+roslaunch --wait circle_drive indefinite_navigation2.launch &
+sleep 5
 # we put a short sleep in here because rostopic will fail if there's no roscore yet
-#rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
+rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
