46d5fb8db7118902cf5ca2561dea6e1ecdb0f3e3
khodozzz <khodozzz@gmail.com>
2021-10-22 17:19:42 +0300

    update

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python3
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from duckietown_msgs.msg import Twist2DStamped
+from navigation.controller import NavigationController
+from navigation import dt_etu
 
-class MyNode(DTROS):
 
+class NavigationNode(DTROS):
     def __init__(self, node_name):
-        super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
-        self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
+        super(NavigationNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
+
+        self.controller = NavigationController(dt_etu.build_graph(), dt_etu.start_pos())
+
+        # self.red_line_sub = rospy.Subcriber("~red_line", Bool, queue_size=1)
+        # self.turn_pub = rospy.Publisher("~turn", Int32, queue_size=1)
+        # self.state_pub = rospy.Publisher("~state", Int32, queue_size=1)
 
     def run(self):
-        # publish message every 1 second
-        rate = rospy.Rate(1) # 1Hz
+        rospy.loginfo('Building path...')
+        self.controller.plan_path(dt_etu.target_pos())
+
         while not rospy.is_shutdown():
-            msg = Twist2DStamped()
-            msg.v = 2.0
-            msg.omega = 2.0
-            rospy.loginfo("Publishing message 0/0.5")
-            self.pub.publish(msg)
-            rate.sleep()
-            msg.v = 0.0
-            msg.omega = 0.0
-            rospy.loginfo("Publishing message 0/0.0")
-            self.pub.publish(msg)
-            rate.sleep()
+            if self.red_line_sub:  # TODO: if on red line
+                # msg = Int32()
+                # msg.data = 0  # turn
+                # self.state_pub.publish(msg)
+
+                turn = self.controller.next_turn()
+                rospy.loginfo(f'Next turn is {turn}')
+                # msg = Int32()
+                # msg.data = turn
+                # self.turn_pub.publish(msg)
+            else:
+                pass
+                # msg = Int32()
+                # msg.data = 1  # lane following
+                # self.state_pub.publish(msg)
+
 
 if __name__ == '__main__':
     # create the node
-    node = MyNode(node_name='circle_drive_node')
+    node = NavigationNode(node_name='navigation_node')
     # run node
     node.run()
     # keep spinning
