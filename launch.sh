a2031e802d3db06c80b086d5cc917df391e3f681
khodozzz <khodozzz@gmail.com>
2021-10-23 12:40:45 +0300

    update

--- launch.sh
+++ launch.sh
@@ -6,6 +6,6 @@ set -e
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
 
-roslaunch --wait circle_drive indefinite_navigation2.launch &
+roslaunch --wait duckietown_dcemos indefinite_navigation.launch &
 sleep 5
 rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
