1f304bb07d99a675e7928f67e841a3e8f3af53c2
khodozzz <khodozzz@gmail.com>
2021-10-25 14:52:10 +0300

    update

--- launch.sh
+++ launch.sh
@@ -5,6 +5,8 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-roslaunch --wait duckietown_demos indefinite_navigation.launch &
-sleep 5
-rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
+roslaunch duckietown_demos indefinite_navigation.launch
+
+#roslaunch --wait duckietown_demos indefinite_navigation.launch &
+#sleep 5
+#rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
